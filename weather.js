5a188c4c155291469ed5ec48f491d745f77d9a1d
petetipaulkiran <petetipaulkiran@gmail.com>
2024-09-02 16:32:27 +0530

    Added the result block

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const apiKey = "f41b0b0ca0ed26d20eb46bb1e4cf37d3";
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?"
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
 
 // API URL -- https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
@@ -8,9 +8,28 @@ buttonEvent = document.querySelector(".content-input button");
 
 async function checkWeather(cityname)
 {
-    const response = await fetch (apiurl+"q="+cityname+"&appid="+apiKey);
+    const response = await fetch (apiurl+"&q="+cityname+"&appid="+apiKey);
     const data  = await response.json();
-    console.log(data.name);
+    if(response.status != 404){
+        // console.log(data.name);
+        // console.log(data.sys.country);
+        // console.log();
+        // console.log(data.weather[0].description);
+        // console.log(data.main.temp_min+" "+data.main.temp_max);
+        document.querySelector('.result-card').style.display = 'flex';
+        document.querySelector('.error-response').style.display = 'none';
+
+        document.querySelector('#city-name').innerHTML = data.name.toUpperCase()+","+data.sys.country;
+        document.querySelector('#city-temperature').innerHTML = data.main.temp+" c";
+        document.querySelector('#city-desc').innerHTML = data.weather[0].description;
+        document.querySelector('#min-max-temp').innerHTML = data.main.temp_min+" "+data.main.temp_max;
+    }
+    else{
+        document.querySelector('.result-card').style.display = 'none';
+        document.querySelector('.error-response').style.display = 'flex';
+        console.log("City Not found");
+    }
+    //document.querySelector("city-name").innerHTML = data.name+","+data.sys.country;
 }
 
 buttonEvent.addEventListener("click",()=>{
