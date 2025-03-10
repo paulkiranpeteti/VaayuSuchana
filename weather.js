5416c76a36169d86c0d8302dc9562417ca08f428
petetipaulkiran <petetipaulkiran@gmail.com>
2025-03-10 12:34:18 +0530

    Added a API Key Var

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+const apiKey = ""
 const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
 
 // API URL -- https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
